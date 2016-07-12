f11c2ad420cba5d9dafa2decc2ee8c6f1a011dfe
maldad <canond_7@hotmail.com>
2016-07-12 07:53:50 -0500

    llegando a la 4ta consulta

---
+++ consultas.sql
@@ -0,0 +1,216 @@
+﻿set search_path to banco;
+
+select "sexo", count("sexo"), ((count("sexo")*100)/100000.0) as "porcentaje" from cliente
+group by "sexo";
+
+select '15 a 17' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 15 and
+date_part('year', age("fnacimiento")) < 18
+
+UNION
+select '18 a 30' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 18 and
+date_part('year', age("fnacimiento")) < 31
+
+UNION
+select '31 a 40' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 31 and
+date_part('year', age("fnacimiento")) < 41
+
+UNION
+select '41 a 60' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 41 and
+date_part('year', age("fnacimiento")) < 60
+
+UNION
+select '60 o mas' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 60
+order by rango;
+
+---------------------------------------------------------------------------------------
+select 
+'F' as sexo,
+'15 a 17' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 15 and
+date_part('year', age("fnacimiento")) < 18 and
+"sexo" = 'F'
+
+UNION
+select 
+'M' as sexo,
+'15 a 17' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 15 and
+date_part('year', age("fnacimiento")) < 18 and
+"sexo" = 'M'
+
+UNION
+select 
+'F' as sexo,
+'18 a 30' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 18 and
+date_part('year', age("fnacimiento")) < 31 and
+"sexo" = 'F'
+
+UNION
+select 
+'M' as sexo,
+'18 a 30' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 18 and
+date_part('year', age("fnacimiento")) < 31 and
+"sexo" = 'M'
+
+UNION
+select 
+'F' as sexo,
+'31 a 40' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 31 and
+date_part('year', age("fnacimiento")) < 41 and
+"sexo" = 'F'
+
+UNION
+select 
+'M' as sexo,
+'31 a 40' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 31 and
+date_part('year', age("fnacimiento")) < 41 and
+"sexo" = 'M'
+
+UNION
+select 
+'F' as sexo,
+'41 a 60' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 41 and
+date_part('year', age("fnacimiento")) < 60 and
+"sexo" = 'F'
+
+UNION
+select 
+'M' as sexo,
+'41 a 60' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 41 and
+date_part('year', age("fnacimiento")) < 60 and
+"sexo" = 'M'
+
+UNION
+select 
+'F' as sexo,
+'60 o mas' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 60 and
+"sexo" = 'F'
+
+UNION
+select 
+'M' as sexo,
+'60 o mas' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+round((count(date_part('year', age("fnacimiento")))*100)/100000.0,3) as porcentaje
+from cliente
+where date_part('year', age("fnacimiento")) >= 60 and
+"sexo" = 'M'
+order by rango, sexo;
+
+
+--total de clientes que han pedido un credito por rango de edad--
+--cambiar cantidad por: total de clientes en ese rango de edad--
+
+select '15 a 17' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+count(c."idCliente") as Creditos
+from cliente c
+INNER JOIN prestamo p
+on c."idCliente" = p."idCliente"
+where date_part('year', age("fnacimiento")) >= 15 and
+date_part('year', age("fnacimiento")) < 18
+
+UNION
+select '18 a 30' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+count(c."idCliente") as Creditos
+from cliente c
+INNER JOIN prestamo p
+on c."idCliente" = p."idCliente"
+where date_part('year', age("fnacimiento")) >= 18 and
+date_part('year', age("fnacimiento")) < 31
+
+UNION
+select '31 a 40' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+count(c."idCliente") as Creditos
+from cliente c
+INNER JOIN prestamo p
+on c."idCliente" = p."idCliente"
+where date_part('year', age("fnacimiento")) >= 31 and
+date_part('year', age("fnacimiento")) < 41
+
+UNION
+select '41 a 60' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+count(c."idCliente") as Creditos
+from cliente c
+INNER JOIN prestamo p
+on c."idCliente" = p."idCliente"
+where date_part('year', age("fnacimiento")) >= 41 and
+date_part('year', age("fnacimiento")) < 60
+
+UNION
+select '60 o mas' as rango, 
+count(date_part('year', age("fnacimiento"))) as cantidad,
+count(c."idCliente") as Creditos
+from cliente c
+INNER JOIN prestamo p
+on c."idCliente" = p."idCliente"
+where date_part('year', age("fnacimiento")) >= 60
+
+
+order by rango;
+
+select c."idCliente", p."idCliente"
+from cliente c
+left join prestamo p
+on c."idCliente" = p."idCliente"
+
+select * from prestamo
+where "idCliente" = 63838
+--solo tenemos 2 clientes que no figuran en la tabla prestamos--
